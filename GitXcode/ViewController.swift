307d435dd32b9ffdfbe21e9e0d9ba6714ea89dbf
dsingh831 <dsingh831@gmail.com>
2016-12-15 20:53:47 -0500

    Added a pointless Array
    Just added another test array called testArray3.

--- GitXcode/ViewController.swift
+++ GitXcode/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     var testArray = [96, 83, 54, 12]
     var testArray2 = [32, 18, 23, 36]
+    var testArray3 = [97, 66, 53, 7]
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
